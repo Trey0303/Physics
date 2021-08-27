be566c525c681005bd95352358464bb6d57a2814
Trey Gleason <s209058@student.aie.edu>
2021-08-26 17:51:22 -0700

    added clipping velocity and gravity

--- Assets/Scripts/KinematicCharacterScripts/KinematicCharacter.cs
+++ Assets/Scripts/KinematicCharacterScripts/KinematicCharacter.cs
@@ -7,31 +7,62 @@ public class KinematicCharacter : MonoBehaviour
     public GameObject player;
     public Rigidbody rb;
     public float speed = 5;
-    public float skinWidth = .05f;
+    public float skinWidth = .001f;
+    Vector3 velocity;
+
+    public bool gravity = false;
+    public bool isGrounded = false;
+
+    public int jumpStrength = 10;
+
+    Vector3 playerSize;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        velocity = rb.velocity;
+        playerSize = player.transform.localScale;
+    }
+
+    private void Update()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Jump");
+                rb.AddForce(transform.up * jumpStrength);
+            }
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        //player.transform.position += velocity * Time.deltaTime;
-        
-        //player movement
+        //update Velocity
+        player.transform.position += velocity * Time.deltaTime;
+       
         //Input
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        
 
+        //gravity
+        if (gravity)
+        {
+            velocity += Physics.gravity * Time.deltaTime;
+        }
+
+        //projected position
+        Vector3 projectedPosition = player.transform.position + input * Time.deltaTime * speed;
 
         var thisCollider = GetComponent<Collider>();//player collider
 
         if (thisCollider != null)
         {
+
             //collide/overlap Detection                                              Skin Width: Add this skin width when performing your collision tests. For example, if my KinematicCharacter has a BoxCollider that has a size of 1,1,1, then I should be testing with a box whose full-extents are 1.001, 1.001, 1.001 when calling Physics.OverlapBox.
-            Collider[] hitColliders = Physics.OverlapBox(player.transform.position, new Vector3(transform.localScale.x + skinWidth, transform.localScale.y + skinWidth, transform.localScale.z + skinWidth), Quaternion.identity);//Remember that Physics.OverlapBox asks for the half-extents so you'll need to divide the box's size by two before passing it along.
+            Collider[] hitColliders = Physics.OverlapBox(projectedPosition, new Vector3(transform.localScale.x + skinWidth, transform.localScale.y + skinWidth, transform.localScale.z + skinWidth), Quaternion.identity);//Remember that Physics.OverlapBox asks for the half-extents so you'll need to divide the box's size by two before passing it along.
 
             //Check when there is a new collider coming into contact with the box
             for (int i = 0; i < hitColliders.Length; i++)
@@ -42,7 +73,7 @@ public class KinematicCharacter : MonoBehaviour
                 }
 
                 //Output all of the collider names
-                Debug.Log("Hit : " + hitColliders[i].name + i);
+                //Debug.Log("Hit : " + hitColliders[i].name + i);
 
                 Vector3 otherPosition = hitColliders[i].transform.position;//gets objects position
                 Quaternion otherRotation = hitColliders[i].transform.rotation;//gets objects rotation
@@ -52,24 +83,26 @@ public class KinematicCharacter : MonoBehaviour
 
                 //ComputePenetration works as a bool to know when the player is or isnt inside an object. Also give you the distance and direction between player and object
                 //                                           object A collider, object A position, object A rotation, object b collider, object b position, object b rotation, MTV(minimum translation vector)
-                bool overlapped = Physics.ComputePenetration(thisCollider, transform.position, transform.rotation, hitColliders[i], otherPosition, otherRotation, out direction, out distance);
+                bool overlapped = Physics.ComputePenetration(thisCollider, projectedPosition, transform.rotation, hitColliders[i], otherPosition, otherRotation, out direction, out distance);
 
                 //if player is gonna overlap with object
                 if (overlapped)
                 {
                     Debug.DrawRay(player.transform.position, direction * distance);
                     //position = position + direction * distance
-                    player.transform.position += direction * distance;//pushes back player by direction times distance and adding to players current position
+                    projectedPosition += direction * distance;//pushes back player by direction times distance and adding to players current position
 
-                    //                                        (vector, planeNormal)
-                    Vector3 clippingVelocity = Vector3.ProjectOnPlane(player.transform.position, direction);
+                    //                               (vector, planeNormal)
+                    velocity = Vector3.ProjectOnPlane(velocity, direction);
+
                 }
             }
 
 
         }
         //Movement
-        rb.MovePosition(player.transform.position + input * Time.deltaTime * speed);
+        rb.MovePosition(projectedPosition);
+        
 
     }
 }
