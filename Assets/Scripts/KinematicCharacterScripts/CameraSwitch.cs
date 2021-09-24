23920531fc5084c76475eb90ef89603da933a0f6
Trey Gleason <s209058@student.aie.edu>
2021-09-24 12:42:51 -0700

    added ui to unity project

--- Assets/Scripts/KinematicCharacterScripts/CameraSwitch.cs
+++ Assets/Scripts/KinematicCharacterScripts/CameraSwitch.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,39 +12,95 @@ public class CameraSwitch : MonoBehaviour
 
     public bool camSwap = false;
 
+    public GameObject playerUI;
+    public GameObject overviewUI;
+    public bool hidden;
+
     // Start is called before the first frame update
     void Start()
     {
         StartingCam();
+        playerUI.SetActive(true);
+        overviewUI.SetActive(false);
+        hidden = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            hideText();
+
+        }
+
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             SwapCam();
         }
     }
 
+    private void hideText()
+    {
+        if (hidden)//text is hidden
+        {
+            hidden = false;
+            //show correct text
+            if (camSwap)//if on player cam show player info
+            {
+                playerUI.SetActive(true);
+                overviewUI.SetActive(false);
+
+            }
+            else if (!camSwap)//if on overview cam show overview info
+            {
+                playerUI.SetActive(false);
+                overviewUI.SetActive(true);
+
+            }
+
+        }
+        else//hide text
+        {
+            hidden = true;
+            playerUI.SetActive(false);
+            overviewUI.SetActive(false);
+
+        }
+
+    }
+
     void SwapCam(){
-        
-            if (camSwap)
+
+        if (camSwap)//overview
+        {
+            cam1.enabled = false;
+            cam2.enabled = true;
+            camSwap = false;
+            player.playerCam = false;
+            if (!hidden)
             {
-                cam1.enabled = false;
-                cam2.enabled = true;
-                camSwap = false;
-                player.playerCam = false;
+                playerUI.SetActive(false);
+                overviewUI.SetActive(true);
 
             }
-            else if (!camSwap)
+
+        }
+        else if (!camSwap)//player
+        {
+            cam1.enabled = true;
+            cam2.enabled = false;
+            camSwap = true;
+            player.playerCam = true;
+
+            if (!hidden)
             {
-                cam1.enabled = true;
-                cam2.enabled = false;
-                camSwap = true;
-                player.playerCam = true;
+                playerUI.SetActive(true);
+                overviewUI.SetActive(false);
 
             }
+
+        }
     }
 
     void StartingCam()
@@ -53,6 +110,8 @@ public class CameraSwitch : MonoBehaviour
             cam1.enabled = true;
             cam2.enabled = false;
             player.playerCam = true;
+            playerUI.SetActive(true);
+            overviewUI.SetActive(false);
 
         }
         else if (!camSwap)//overview cam
@@ -60,6 +119,8 @@ public class CameraSwitch : MonoBehaviour
             cam1.enabled = false;
             cam2.enabled = true;
             player.playerCam = false;
+            playerUI.SetActive(false);
+            overviewUI.SetActive(true);
 
         }
     }
